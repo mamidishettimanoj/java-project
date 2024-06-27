a4f50a7f513f26cf3524dd27c28525b05b77b2a3
mamidishettimanoj <manojmamidishetti245@gmail.com>
2024-06-27 12:19:54 +0530

    frontend project

---
+++ react_frontend/src/components/FooterComponent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import  '../App.css';
+
+function FooterComponent() {
+  return (
+    <div>
+          <footer className='footer'>
+            <span>All Right Reserved 2024 &copy;NIYAZ_SIR </span>
+          </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
